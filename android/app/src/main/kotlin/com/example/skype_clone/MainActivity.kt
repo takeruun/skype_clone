cb7ecebdd5bb0fcc4a02dadaab52711e07c5fae9
takeru.hashimoto <hstake25@gmail.com>
2021-02-23 17:13:50 +0900

    android push通知受信

--- android/app/src/main/kotlin/com/example/skype_clone/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/skype_clone/MainActivity.kt
@@ -1,6 +1,15 @@
 package com.example.skype_clone
 
+import android.os.Bundle
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.PluginRegistry
+import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.view.FlutterMain
 
 class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
 }
